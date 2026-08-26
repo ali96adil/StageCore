78d8d86adc4d7fd814fbb2acf2897e5d424b0b15
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-08-26 14:23:33 +0300

    m1: advance schema migration test to version 2

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -24,8 +24,8 @@ func TestOpenAppliesMigrationAndRequiredPragmas(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if version != 1 {
-		t.Fatalf("schema version=%d, want 1", version)
+	if version != 2 {
+		t.Fatalf("schema version=%d, want 2", version)
 	}
 }
 
