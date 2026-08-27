647c753d6dd5065dfdeefbab0d8d9a48ce9b0de1
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-08-27 16:08:30 +0300

    test: import note authorization package

--- internal/httpapi/operator_memory_test.go
+++ internal/httpapi/operator_memory_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ali96adil/StageCore/internal/sessionmemory"
 	"github.com/ali96adil/StageCore/internal/snapshot"
 	"github.com/ali96adil/StageCore/internal/store"
+	"github.com/ali96adil/StageCore/internal/userauth"
 )
 
 func TestOperatorNotesAndSessionMemoryAPI(t *testing.T) {
