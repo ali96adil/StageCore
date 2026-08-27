64b0513610ff14cdc81d847d4b341b4096bbeb37
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-08-27 17:33:40 +0300

    m6: serve routing configuration client

--- internal/httpapi/operator_web.go
+++ internal/httpapi/operator_web.go
@@ -17,6 +17,7 @@ func WithOperatorWeb() Option {
 		s.mux.HandleFunc("GET /preflight.js", serveOperatorAsset("preflight.js", "application/javascript; charset=utf-8", false))
 		s.mux.HandleFunc("GET /memory.js", serveOperatorAsset("memory.js", "application/javascript; charset=utf-8", false))
 		s.mux.HandleFunc("GET /security.js", serveOperatorAsset("security.js", "application/javascript; charset=utf-8", false))
+		s.mux.HandleFunc("GET /configuration.js", serveOperatorAsset("configuration.js", "application/javascript; charset=utf-8", false))
 	}
 }
 
