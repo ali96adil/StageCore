43c2edca0dd6c9a46ef59f9734dce8ff869e2ed4
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-08-27 14:23:39 +0300

    test: expect web auth migration

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -24,8 +24,8 @@ func TestOpenAppliesMigrationAndRequiredPragmas(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if version != 8 {
-		t.Fatalf("schema version=%d, want 8", version)
+	if version != 9 {
+		t.Fatalf("schema version=%d, want 9", version)
 	}
 }
 
