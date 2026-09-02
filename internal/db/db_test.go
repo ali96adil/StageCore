079c992ef578a2dd1ae525c3a49fbed1b6a4c067
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-09-02 09:22:00 +0300

    test: expect execution environment binding schema

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -24,8 +24,8 @@ func TestOpenAppliesMigrationAndRequiredPragmas(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if version != 20 {
-		t.Fatalf("schema version=%d, want 20", version)
+	if version != 21 {
+		t.Fatalf("schema version=%d, want 21", version)
 	}
 }
 
