5515348b0389c04d35c7eaeccbfe06eee49564e1
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-09-18 10:39:41 +0300

    ESP32 DMX Slice 2: advance schema expectation

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -24,8 +24,8 @@ func TestOpenAppliesMigrationAndRequiredPragmas(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if version != 27 {
-		t.Fatalf("schema version=%d, want 27", version)
+	if version != 28 {
+		t.Fatalf("schema version=%d, want 28", version)
 	}
 }
 
