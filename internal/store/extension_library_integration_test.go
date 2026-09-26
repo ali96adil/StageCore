ba0d4755706072588a1c7f682e3b5c5b96490623
ali96adil <70801949+ali96adil@users.noreply.github.com>
2026-09-26 12:30:57 +0300

    test: expect StageCore schema version 37

--- internal/store/extension_library_integration_test.go
+++ internal/store/extension_library_integration_test.go
@@ -57,8 +57,8 @@ func TestExtensionLibraryPersistsVerifiedPackageAcrossRestart(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if version != 36 {
-		t.Fatalf("schema version=%d want=36", version)
+	if version != 37 {
+		t.Fatalf("schema version=%d want=37", version)
 	}
 	if err := h.Close(); err != nil {
 		t.Fatal(err)
